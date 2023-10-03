10d6ad8c4d90746a49c9964e8f05c97e2e346ea5
unknown <medeirosartur48@gmail.com>
2023-10-03 16:29:59 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./styles/global";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle/>
+    </div>
+  );
+}
+
+export default App;
