7159fd93193f488dbf66e92dbf0d8d28371702f1
arthurzop <medeirosartur48@gmail.com>
2023-10-09 21:20:45 -0300

    vid 18:14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import GlobalStyle from "./styles/global";
+import styled from "styled-components";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 880px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <Container>
+          <Title>Usuarios</Title>
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
